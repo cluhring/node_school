5ad3de402ee61213af4e729fa90215687af1d310
Luhring <cluhring@ms.ds.uhc.com>
2016-02-15 15:19:34 -0700

    my_first_io completed & notes added

--- baby_steps.js
+++ baby_steps.js
@@ -1,8 +1,11 @@
-var nums = process.argv;
+// process.argv = access command-line arguments via the global 'process' object.
+// the 'process' object has an argv property which is an array that contains node, path, and arguments
+var num_array = process.argv;
 var sum = 0;
-
-for (i = 2; i < nums.length; i++) {
-  sum += parseInt(nums[i]);
+// loop through array, starting at arguments/numbers and add them
+for (i = 2; i < num_array.length; i++) {
+  sum += parseInt(num_array[i]);
+  // sum += Number(num_array[i]);
 }
 
 console.log(sum);
---
+++ my_first_io.js
@@ -0,0 +1,17 @@
+// require node's filesystem module
+var fs = require('fs');
+// accept one command-line arguement
+var file = process.argv[2];
+// All synchronous filesystem methods in fs module end with 'Sync'
+// readFileSync method returns a Buffer object containing contents of file
+// see fs module documentation for more deets
+file_content = fs.readFileSync(file)
+// buffer objects can be converted to strings using .toString()
+// or you can also use a 'utf' flag arguement
+file_string = file_content.toString();
+// split string into array at new line delimiter
+lines = file_string.split('\n');
+// subtract one as last line doesn't have the delimiter
+file_length = lines.length - 1;
+// global output the file_length
+console.log(file_length);
