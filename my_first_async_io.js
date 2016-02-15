e8a1ce2a8bc8cdaf043030c3c8f605c29f4963d3
Luhring <cluhring@ms.ds.uhc.com>
2016-02-15 16:32:13 -0700

    my_first_async_io complete

---
+++ my_first_async_io.js
@@ -0,0 +1,10 @@
+var fs = require('fs');
+var file = process.argv[2];
+// readFile is async method that requires a callback function
+// that function checks for errors, looking for truthy arg
+// if no error, returns string, which is split & counted :)
+fs.readFile(file, 'utf8', function(err, string_file) {
+  lines = string_file.split('\n');
+  line_count = lines.length - 1;
+  console.log(line_count);
+});
