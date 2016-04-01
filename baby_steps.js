3ea3ab41cebf674dce8702ca21d12d2152e58f62
Chris Luhring <cluhring@gmail.com>
2016-03-31 19:09:04 -0600

    added more notes to baby_steps.js

--- baby_steps.js
+++ baby_steps.js
@@ -3,13 +3,15 @@
 var num_array = process.argv;
 var sum = 0;
 // loop through array, starting at arguments/numbers and add them
+// parseInt() function parses a string argument and returns an intege
+// Number() function converts different object values to their numbers: http://www.w3schools.com/jsref/jsref_number.asp
+// Number vs parseInt: http://stackoverflow.com/questions/4090518/what-is-the-difference-between-parseint-and-number
 for (i = 2; i < num_array.length; i++) {
   sum += parseInt(num_array[i]);
   // sum += Number(num_array[i]);
 }
 
 console.log(sum);
-
 
 // this program sums arguments entered into the command line
 // and returns them via console.log
